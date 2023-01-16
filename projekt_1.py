e1cedda07042e49382ecc278f67cef21e2b35c48
MarekPosmura <m.posmura@seznam.cz>
2023-01-16 23:50:26 +0100

    zahájení projektu 1-textový_analyzátor

---
+++ projekt_1.py
@@ -0,0 +1,10 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Marek Pošmura
+email: m.posmura@seznam.cz
+discord: Marek P. #5306
+"""
+import task_template
+
+print(task_template.TEXTS[1])
