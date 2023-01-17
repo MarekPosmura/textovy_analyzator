5b7e3def52fbd8f1786bb96ebcb22be41579725a
MarekPosmura <m.posmura@seznam.cz>
2023-01-17 20:49:49 +0100

    přihlášení uživatele

--- projekt_1.py
+++ projekt_1.py
@@ -7,4 +7,26 @@ discord: Marek P. #5306
 """
 import task_template
 
-print(task_template.TEXTS[1])
+# print(task_template.TEXTS[1])
+
+users = {
+    "bob":"123",
+    "ann":"pass123",
+    "mike":"password123",
+    "liz":"pass123"
+    }
+line = 40*"-"
+
+print("Welcome to the text analyzer!")
+username = input("Enter your username:")
+password = input("Enter your password:")
+
+# ověřit uzivatele a heslo
+if users.get(username) == password:
+    print(line)
+    print(f"Welcome to the app, {username}.\nWe have 3 texts to be analyzed.")
+else:
+    print("Unregistered user, terminating the program...")
+    quit()
+
+print(line)
